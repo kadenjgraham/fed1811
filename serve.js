e23ee48caeb7be79a5e675edddb5082bb8011be8
Kaden <kadenjgraham@yahoo.com>
2019-01-04 10:46:09 -0500

    adding heroku

--- serve.js
+++ serve.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const hbs = require('hbs');
 let serverApp = express();
+const port = process.env.PORT || 3000;
 serverApp.use(express.static(`${__dirname}/public`));
 serverApp.get('/', (req, res) => {
     res.render('home.hbs', {
 
     });
 });
-serverApp.listen(3000, () => {
-    console.log('Your express app is being served on port 3000!');
+serverApp.listen(port, () => {
+    console.log(`Your express app is being served on port ${port}!`);
 });
